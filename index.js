dc2ce4805942b9714a907f351512623e48e4b535
NBS5000 <steve.nbs.barry@gmail.com>
2022-02-03 00:39:44 +1100

    Built md template, not expecting this to work, but lets see....

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
+const fileName = "README.md";
 
 // TODO: Create an array of questions for user input
 const questions = [
+    console.log("If you need help with a question, simply press return without any input"),
     {
         type: "input",
         message: "What is the title of your project?",
@@ -30,8 +32,41 @@ const questions = [
     },
     {
         type: "input",
+        message: "AS A ........",
+        name: "asa",
+        validate(answer) {
+            if(!answer) {
+                return "The User Story.....what is the role of the user?"
+            }
+            return true
+        }
+    },
+    {
+        type: "input",
+        message: "I WANT ........",
+        name: "iwant",
+        validate(answer) {
+            if(!answer) {
+                return "The User Story Strikes Back!.....what do you want to achieve?"
+            }
+            return true
+        }
+    },
+    {
+        type: "input",
+        message: "SO THAT ........",
+        name: "sothat",
+        validate(answer) {
+            if(!answer) {
+                return "Return of the The User Story .....how will this make the users life easier?"
+            }
+            return true
+        }
+    },
+    {
+        type: "input",
         message: "What is the intended usage of the project?",
-        name: "title",
+        name: "usage",
         validate(answer) {
             if(!answer) {
                 return "You do need to say what the usage is!"
@@ -39,6 +74,46 @@ const questions = [
             return true
         }
     },
+    {
+        type: "input",
+        message: "What is the end goal of this project?",
+        name: "goal",
+        validate(answer) {
+            if(!answer) {
+                return "What will this project achieve?"
+            }
+            return true
+        }
+    },
+    {
+        type: "input",
+        message: "What did you do to achieve the end goal?",
+        name: "action",
+        validate(answer) {
+            if(!answer) {
+                return "List the tasks and actions you did to achieve the end result"
+            }
+            return true
+        }
+    },
+    {
+        type: "input",
+        message: "How did it go? Were the targets met? How so?",
+        name: "result",
+        validate(answer) {
+            if(!answer) {
+                return "List the tasks and actions you did to achieve the end result"
+            }else if(answer.length < 30){
+                return "You'll need to be a bit more detailed than that."
+            }
+            return true
+        }
+    },
+
+
+
+
+    console.log("And now the technical stuff"),
     {
         type: "list",
         message: "What type of license does the project have?",
@@ -79,18 +154,77 @@ const questions = [
     },
 ];
 
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function build(data) {
+    let theCode = ({license,title,desc,asa,iwant,sothat,usage,goal,action,result,git}) =>
+    `
+    ${license}
+    ## ${title}
+    
+    ## <span style="color:green">The Description</span>
+    
+    ${desc}
+    
+    ### ⭐ ⭐ S.T.A.R. ⭐ ⭐
+    
+    **SITUATION**
+    
+    The provided user story was: 
+    
+    > <span style="font-style:italic">AS A ${asa}</span>
+    > 
+    > <span style="font-style:italic">I WANT ${iwant}</span>
+    >
+    > <span style="font-style:italic">SO THAT ${sothat}</span>
+    
+    **TASK**
+    
+    ${usage}
+    
+    ${goal}
+    
+    **ACTION**
+    
+    ${action}
+    
+    **RESULT**
+    
+    ${result}
+    
+    ## <span style="color:green"> The Outcome</span>
+    
+    You can view the finished product [HERE](https://${git}.github.io/${title}/)
+    
+    Alternatively, here is a screenshot:
+    
+    > ![Screenshot of The Weather page](./assets/images/screen.png "Screenshot of The Weather page")
+
+    If you have any questions regarding this, feel free to contact me at [${email}](mailto:${email})
+
+    
+    <p style="text-align:center;">${git}</p>
+    <p style="text-align:center;">[![Github icon](https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png)](https://github.com/${git})</p>
+    
+    ---
+    
+
+    `
+    return theCode;
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
     .prompt(questions)
     .then((data) => {
-        const filename = "README.md";
-        // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-        // err ? console.log(err) : console.log('Huzzah!')
-        // );
+        build(data)
+    })
+    .then((code) => {
+        fs.writeFile(fileName, code, (err) =>
+        err ? console.log(err) : console.log('Huzzah!')
+        );
     });
 }
 
