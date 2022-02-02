248e41450e63866f5be04b4f783dbca51e1f140e
NBS5000 <steve.nbs.barry@gmail.com>
2022-02-02 23:47:12 +1100

    testing code, correctly runs through questions and generates file in basic form

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -7,21 +8,40 @@ const questions = [
         type: "input",
         message: "What is the title of your project?",
         name: "title",
-    },
+        validate(answer) {
+            if(!answer) {
+                return "Please enter the name of your project!"
+            }
+            return true
+        }
+    },    
     {
         type: "input",
-        message: `Please describe the ${title}`,
+        message: "Please describe the project",
         name: "description",
-        
+        validate(answer) {
+            if(!answer) {
+                return "A description isn't too hard, is it?"
+            }else if(answer.length < 30){
+                return "Please enter a bit more detail, at least 30 characters!"
+            }
+            return true
+        }
     },
     {
         type: "input",
-        message: `What is the intended usage of ${title}?`,
+        message: "What is the intended usage of the project?",
         name: "title",
+        validate(answer) {
+            if(!answer) {
+                return "You do need to say what the usage is!"
+            }
+            return true
+        }
     },
     {
         type: "list",
-        message: `What type of licence does ${title} have?`,
+        message: "What type of license does the project have?",
         choices: [
             "GNU AGPLv3",
             "GNU GPLv3",
@@ -32,22 +52,30 @@ const questions = [
             "Boost Software License 1.0",
             "The Unlicense",
         ],
-        name: 'licence',
+        name: 'license',
     },
     {
         type: "input",
-        message: "What is the title of your project?",
-        name: "title",
+        message: "What is your Github name?",
+        name: "github",
+        validate(answer) {
+            if(!answer) {
+                return "Don 't make me repeat myself!"
+            }
+            return true
+        }
     },
     {
         type: "input",
-        message: "What is the title of your project?",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title",
+        message: "what is your email address?",
+        name: "email",
+        validate: (answer) => {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            if(!emailRegex.test(answer)) {
+                return "You have to provide a valid email address!"
+            }
+            return true
+        }
     },
 ];
 
@@ -55,7 +83,16 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        const filename = "README.md";
+        // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+        // err ? console.log(err) : console.log('Huzzah!')
+        // );
+    });
+}
 
 // Function call to initialize app
 init();
